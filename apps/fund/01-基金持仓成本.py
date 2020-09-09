4938455bed28fc10e08b1bb3fb116b67d5daed98
kang.yang <kang.yang@b-and-qchina.com>
2020-09-09 16:20:46 +0800

    基金的持仓成本

---
+++ apps/fund/01-基金持仓成本.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ python3
+# -*- coding:utf-8 -*-
+
+def addFund(objectNet):
+    # 原有基金金额
+    oldAmount = 25324.63
+    # 原有基金净值
+    oldNet = 3.1745
+    #
+    predictNet = 2.8575
+    amount = 1000
+    while True:
+        newNet = (oldAmount + amount) / (oldAmount / oldNet + amount / predictNet)
+        if newNet <= objectNet:
+            break
+        else:
+            print("amount = ", amount)
+            print("net = ", newNet)
+            amount += 500
+    print("最终需要投资 amount = ", amount)
+
+
+addFund(2.9)
